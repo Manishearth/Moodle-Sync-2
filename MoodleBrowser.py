909ee321ff83a20316510752168bd579fcc1ea4c
Manish Goregaokar <manishsmail@gmail.com>
2013-08-15 13:48:44 +0530

    login

--- MoodleBrowser.py
+++ MoodleBrowser.py
@@ -1,6 +1,27 @@
 from BeautifulSoup import *
 from mechanize import *
+import ConfigParser
+import json
 
 class MoodleBrowser:
 	def __init__(self):
+		self.b=Browser()
+		self.b.set_handle_robots(False)
+		self.conf=json.loads(open('conf.json').read())
 		
+	def login(self,user,passwd):
+		self.b.open(self.conf['site'])
+		self.b.select_form(nr=1)
+		for i in range(3):
+			self.b.form.controls[i].readonly = False
+		self.b["username"] = user
+		self.b["password"] = passwd
+		print "submitting"
+		resp = self.b.submit()
+		if '/login/' in br.geturl():
+			return False
+		return True
+		
+
+a=MoodleBrowser()
+a.login('manish','manish')
